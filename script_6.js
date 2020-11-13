af8baaf35769a716e225ed1c235a8d1dde82199e
florianempinet <fempinet@gmail.com>
2020-11-13 08:51:02 +0100

    Final

--- script_6.js
+++ script_6.js
@@ -8,19 +8,19 @@ chain.forEach((arn) => {
     if (arn == "UCU" || arn == "UCC" || arn == "UCA" || arn == "UCG" || arn == "AGU" || arn == "AGC") {
         arn1.push("Sérine")
     }
-    if (arn == "CCU" || arn == "CCC" || arn == "CCA" || arn == "CCG") {
+    else if (arn == "CCU" || arn == "CCC" || arn == "CCA" || arn == "CCG") {
         arn1.push("Proline")
     }
-    if (arn == "UUA" ||  arn == "UUG") {
+    else if (arn == "UUA" ||  arn == "UUG") {
         arn1.push("Leucine")
     }
-    if (arn == "UUU" || arn == "UUC") {
+    else if (arn == "UUU" || arn == "UUC") {
         arn1.push("Phénylalaline")
     }
-    if (arn == "CGU" || arn == "CGC" || arn == "CGA" || arn == "CGG" || arn == "AGA" || arn == "AGG") {
+    else if (arn == "CGU" || arn == "CGC" || arn == "CGA" || arn == "CGG" || arn == "AGA" || arn == "AGG") {
         arn1.push("Arginine")
     }
-    if (arn == "UAU" || arn == "UAC") {
+    else if (arn == "UAU" || arn == "UAC") {
         arn1.push("Tyrosine")
     }
 })
@@ -32,22 +32,22 @@ var arn2 = []
 
 var chain = acid2.split(/(?=(?:...)*$)/)
 chain.forEach((arn) => {
-    if (arn == "UCU" || arn == "UCC" || arn == "UCA" || arn == "UCG" || arn == "AGU" || arn == "AGC") {
+     if (arn == "UCU" || arn == "UCC" || arn == "UCA" || arn == "UCG" || arn == "AGU" || arn == "AGC") {
         arn2.push("Sérine")
     }
-    if (arn == "CCU" || arn == "CCC" || arn == "CCA" || arn == "CCG") {
+    else if (arn == "CCU" || arn == "CCC" || arn == "CCA" || arn == "CCG") {
         arn2.push("Proline")
     }
-    if (arn == "UUA" ||  arn == "UUG") {
+    else if (arn == "UUA" ||  arn == "UUG") {
         arn2.push("Leucine")
     }
-    if (arn == "UUU" || arn == "UUC") {
+    else if (arn == "UUU" || arn == "UUC") {
         arn2.push("Phénylalaline")
     }
-    if (arn == "CGU" || arn == "CGC" || arn == "CGA" || arn == "CGG" || arn == "AGA" || arn == "AGG") {
+    else if (arn == "CGU" || arn == "CGC" || arn == "CGA" || arn == "CGG" || arn == "AGA" || arn == "AGG") {
         arn2.push("Arginine")
     }
-    if (arn == "UAU" || arn == "UAC") {
+    else if (arn == "UAU" || arn == "UAC") {
         arn2.push("Tyrosine")
     }
 })
